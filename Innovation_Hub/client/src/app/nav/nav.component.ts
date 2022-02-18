39c804530af1926c5a5fa700a84df37c31c0af3f
Wendel S <wendel.simoes.br@gmail.com>
2022-02-18 20:58:38 -0300

    Setting up login structure

--- Innovation_Hub/client/src/app/nav/nav.component.ts
+++ Innovation_Hub/client/src/app/nav/nav.component.ts
@@ -1,22 +1,34 @@
 import { Component, TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { AccountService } from '../_services/account.service';
  
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-nav',
-  templateUrl: './nav.component.html'
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
   modalRef?: BsModalRef;
-  constructor(private modalService: BsModalService) {}
+  constructor(private modalService: BsModalService, private accountService: AccountService) {}
 
   model: any = {}
+  loggedIn: boolean;
  
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
 
   login() {
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+      this.loggedIn = true;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  logout() {
+    this.loggedIn = false;
   }
 }
