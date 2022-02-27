db0e41d13a662a7e21676bcc0592b111469b82a4
Wendel S <wendel.simoes.br@gmail.com>
2022-02-26 21:26:49 -0300

    Ongoing

--- Innovation_Hub/API/Entities/InterestArea.cs
+++ Innovation_Hub/API/Entities/InterestArea.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,23 +19,41 @@ namespace API.Entities
         espacos*/
         public enum InterestAreaEnum
         {
+            [Description("Arte e Cultura")]
             Arte_e_Cultura,
+            [Description("Música e Entretenimento")]
             Musica_e_Entretenimento,
+            [Description("Automóveis e Veiculos")]
             Automoveis_e_Veiculos,
+            [Description("Informática e Eletronica")]
             Informatica_e_Eletronica,
+            [Description("Educação")]
             Educacao,
+            [Description("Vida")]
             Vida,
+            [Description("Família")]
             Familia,
+            [Description("Negócios e Empreendedorismo")]
             Negocios_e_Empreendedorismo,
+            [Description("Culinaria e Gastronomia")]
             Culinaria_e_Gastronomia,
+            [Description("Saúde e Bem Estar")]
             Saude_e_Bem_Estar,
+            [Description("Esporte")]
             Esporte,
+            [Description("Viagem e Turismo")]
             Viagem_e_Turismo,
+            [Description("Economia e Finanças")]
             Economia_e_Financas,
+            [Description("Política e Mundo")]
             Politica_e_Mundo,
+            [Description("Ciência e Tecnologia")]
             Ciencia_e_Tecnologia,
+            [Description("Trabalho e Carreira")]
             Trabalho_e_Carreira,
+            [Description("Psicologia e Sociedade")]
             Psicologia_e_Sociedade,
+            [Description("Meio Ambiente")]
             Meio_Ambiente
         }
 
