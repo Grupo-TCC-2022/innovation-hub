5d7bd35e4599acbca5aac3bbc51feedbb229510d
wendelsimoes <wendel.simoes.br@gmail.com>
2022-03-13 10:55:40 -0300

    Making feed page

--- Innovation_Hub/client/src/app/feed/feed-card/feed-card.component.ts
+++ Innovation_Hub/client/src/app/feed/feed-card/feed-card.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { faArrowCircleUp as arrowSolid } from '@fortawesome/free-solid-svg-icons';
+import { faArrowAltCircleUp as arrowRegular } from '@fortawesome/free-regular-svg-icons';
+import { faStar as starFavoriteSolid } from '@fortawesome/free-solid-svg-icons';
+import { faStar as starFavoriteRegular } from '@fortawesome/free-regular-svg-icons';
+import { InterestAreaService } from 'src/app/_services/interest-area.service';
 
 @Component({
   selector: 'app-feed-card',
@@ -6,8 +11,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed-card.component.css']
 })
 export class FeedCardComponent implements OnInit {
+  @Input() proposal: any;
+  arrowSolid = arrowSolid;
+  arrowRegular = arrowRegular;
+  starFavoriteSolid = starFavoriteSolid;
+  starFavoriteRegular = starFavoriteRegular;
 
-  constructor() { }
+  constructor(public interestAreaService: InterestAreaService) { }
+
+  toggleVote(event: any) {
+    //A fazer
+    document.getElementById("openModalBtn").click();
+  }
 
   ngOnInit(): void {
   }
